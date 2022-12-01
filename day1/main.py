0e229858fddef103e2a3b4bb707576448380b4f5
PepperLola <PepperLola@users.noreply.github.com>
2022-12-01 09:26:56 -0800

    :rocket: initial commit

---
+++ day1/main.py
@@ -0,0 +1,55 @@
+import sys
+import time
+
+sums = []
+
+def p1(lines):
+    curr_sum = 0
+    for line in lines:
+        if line != "\n":
+            curr_sum += int(line)
+        else:
+            sums.append(curr_sum)
+            curr_sum = 0
+    sums.sort()
+    sums.reverse()
+    return sums[0]
+
+def p2(lines):
+    return sums[0] + sums[1] + sums[2]
+
+filename = "input.txt"
+
+if "test" in sys.argv:
+    filename = "test.txt"
+
+def format_time(time_ns):
+    names = ["hr", "m", "s", "ms", "µs", "ns"]
+    names.reverse()
+    times = [
+        time_ns % 1000,
+        (time_ns // 1000) % 1000,
+        (time_ns // (1000 * 10**3)) % 1000,
+        (time_ns // (1000 * 10**6)) % 60,
+        (time_ns // (1000 * 10**6) // 60) % 60,
+        (time_ns // (1000 * 10**6) // 60 // 60) % 60
+    ]
+    for i in range(0, len(times)):
+        if i < len(times) - 1:
+            if times[i + 1] == 0:
+                return "%s%s " % (times[i], names[i])
+        else:
+            return "%s%s " % (times[i], names[i])
+
+with open(filename, "r") as f:
+    lines = f.readlines()
+    t = time.perf_counter_ns()
+    a = p1(lines)
+    dur = time.perf_counter_ns() - t
+
+    print("\033[0;33m├ Part 1:\033[22m\033[49m \033[1;93m%s\033[0m \033[3mtook\033[23m \033[3;92m%s\033[0m" % (a, format_time(dur)))
+
+    t = time.perf_counter_ns()
+    a = p2(lines)
+    dur = time.perf_counter_ns() - t
+    print("\033[0;33m└ Part 2:\033[22m\033[49m \033[1;93m%s\033[0m \033[3mtook\033[23m \033[3;92m%s\033[0m" % (a, format_time(dur)))
