08b1b85f9bc6ec17254e363b0351670a9aa2b18a
PepperLola <PepperLola@users.noreply.github.com>
2022-12-05 12:55:33 -0800

    finished 2015 day 7

--- 2015/day7/main.py
+++ 2015/day7/main.py
@@ -1,16 +1,64 @@
 import sys
 import time
 import re
+from collections import namedtuple
 
 # shared variables here
+wires = {}
+
+def init_wires(lines):
+    for line in lines:
+        if " -> " in line:
+            match = re.search(r"(.+) -> ([\w\d]+)", line)
+            wires[match.group(2)] = match.group(1)
+
+def process_op(op, val1, val2 = 0):
+    if op == "NOT":
+        return ~val1
+    elif op == "AND":
+        return val1 & val2
+    elif op == "OR":
+        return val1 | val2
+    elif op == "LSHIFT":
+        return val1 << val2
+    elif op == "RSHIFT":
+        return val1 >> val2
+
+def evaluate_wire(wire):
+    if isinstance(wire, int) or wire.isnumeric():
+        return int(wire)
+    inst = wires[wire]
+    if isinstance(inst, int) or inst.isnumeric():
+        return int(inst)
+
+    if not " " in inst: #just direct wire set
+        wires[wire] = evaluate_wire(inst)
+    else:
+        if "NOT" in inst:
+            match = re.search(r"^NOT ([\w\d]+)", inst)
+            if  match:
+                wires[wire] = process_op("NOT", evaluate_wire(match.group(1)))
+        else:
+            match = re.search(r"^([\w\d]+) (\w+) ([\w\d]+)", inst)
+            if match:
+                wire1 = match.group(1)
+                wire2 = match.group(3)
+                op = match.group(2)
+                wires[wire] = process_op(op, evaluate_wire(wire1), evaluate_wire(wire2))
+    return wires[wire]
 
 # part 1, takes in lines of file
 def p1(lines):
-    return 0
+    init_wires(lines)
+    return evaluate_wire("a")
+
 
 # part 2, takes in lines of file
 def p2(lines):
-    return 0
+    a = evaluate_wire("a")
+    init_wires(lines)
+    wires['b'] = a
+    return evaluate_wire("a")
 
 filename = "input.txt"
 
