89eb8e96669e62f2fbff847c27c5909cd29c3122
PepperLola <PepperLola@users.noreply.github.com>
2022-12-05 21:10:54 -0800

    finished day 6

---
+++ 2022/day6/main.py
@@ -0,0 +1,73 @@
+import sys
+import time
+import re
+
+# shared variables here
+
+# part 1, takes in lines of file
+def p1(lines):
+    line = lines[0]
+    for i in range(len(line) - 3):
+        four = line[i:i+4]
+        unique = True
+        for c in four:
+            arr = [k for k in four if k == c]
+            if len(arr) > 1:
+                unique = False
+
+        if unique:
+            return i + 4
+        
+    return 0
+
+# part 2, takes in lines of file
+def p2(lines):
+    line = lines[0]
+    for i in range(len(line) - 3):
+        four = line[i:i+14]
+        unique = True
+        for c in four:
+            arr = [k for k in four if k == c]
+            if len(arr) > 1:
+                unique = False
+
+        if unique:
+            return i + 14
+
+    return 0
+
+filename = "input.txt"
+
+if "test" in sys.argv:
+    filename = "test.txt"
+
+def format_time(time_ns):
+    names = ["hr", "m", "s", "ms", "µs", "ns"]
+    names.reverse()
+    times = [
+        time_ns % 1000,
+        (time_ns // 1000) % 1000,
+        (time_ns // (1000 * 10**3)) % 1000,
+        (time_ns // (1000 * 10**6)) % 60,
+        (time_ns // (1000 * 10**6) // 60) % 60,
+        (time_ns // (1000 * 10**6) // 60 // 60) % 60
+    ]
+    for i in range(0, len(times)):
+        if i < len(times) - 1:
+            if times[i + 1] == 0:
+                return "%s%s " % (times[i], names[i])
+        else:
+            return "%s%s " % (times[i], names[i])
+
+with open(filename, "r") as f:
+    lines = f.readlines()
+    t = time.perf_counter_ns()
+    a = p1(lines)
+    dur = time.perf_counter_ns() - t
+
+    print("\033[0;33m├ Part 1:\033[22m\033[49m \033[1;93m%s\033[0m \033[3mtook\033[23m \033[3;92m%s\033[0m" % (a, format_time(dur)))
+
+    t = time.perf_counter_ns()
+    a = p2(lines)
+    dur = time.perf_counter_ns() - t
+    print("\033[0;33m└ Part 2:\033[22m\033[49m \033[1;93m%s\033[0m \033[3mtook\033[23m \033[3;92m%s\033[0m" % (a, format_time(dur)))
