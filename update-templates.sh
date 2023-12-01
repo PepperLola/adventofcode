4be4ebaaeb48d6c77ed888b7656a5b1c823d9f81
PepperLola <PepperLola@users.noreply.github.com>
2023-11-30 17:37:22 -0800

    update shell scripts in each folder along with template dir

--- update-templates.sh
+++ update-templates.sh
@@ -1,5 +1,15 @@
 for dir in */; do
+    dir="${dir%/}"
     echo "Copying template to $dir..."
-    [ "${dir%/}" != "template" ] && rm -r "$dir/template" && rm -r "$dir/*.sh"
-    rsync -avr template/template "$dir"
+    if [ "$dir" != "template" ]; then
+        echo "$dir is not template"
+        if [ -d "$dir/template" ]; then
+            rm -r "$dir/template"
+        fi
+        for f in "$dir"/*.sh; do
+            rm "$f"
+        done
+    fi
+    rsync -ar template/template "$dir"
+    rsync -ar template/*.sh "$dir"
 done
