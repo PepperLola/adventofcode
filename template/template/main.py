557f6e9f05d089f81322ed9c3b12ae18d7671794
PepperLola <PepperLola@users.noreply.github.com>
2023-11-30 20:55:32 -0800

    added additional grid util variants and pass file, contents, and lines into each part function

--- template/template/main.py
+++ template/template/main.py
@@ -6,16 +6,16 @@ from collections import defaultdict, deque
 import math
 import copy
 import numpy as np
-from util import format_time
+from util import format_time, number_grid
 
 # shared variables here
 
 # part 1, takes in lines of file
-def p1(lines):
+def p1(file, content, lines):
     return 0
 
 # part 2, takes in lines of file
-def p2(lines):
+def p2(file, content, lines):
     return 0
 
 filename = "input.txt"
@@ -24,14 +24,15 @@ if "test" in sys.argv:
     filename = "test.txt"
 
 with open(filename, "r") as f:
-    lines = f.read().splitlines()
+    content = f.read()
+    lines = content.splitlines()
     t = time.perf_counter_ns()
-    a = p1(lines)
+    a = p1(f, content, lines)
     dur = time.perf_counter_ns() - t
 
     print("\033[0;33m├ Part 1:\033[22m\033[49m \033[1;93m%s\033[0m \033[3mtook\033[23m \033[3;92m%s\033[0m" % (a, format_time(dur)))
 
     t = time.perf_counter_ns()
-    a = p2(lines)
+    a = p2(f, content, lines)
     dur = time.perf_counter_ns() - t
     print("\033[0;33m└ Part 2:\033[22m\033[49m \033[1;93m%s\033[0m \033[3mtook\033[23m \033[3;92m%s\033[0m" % (a, format_time(dur)))
--- template/template/util.py
+++ template/template/util.py
@@ -2,27 +2,39 @@ import numpy as np
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-def letter_grid(file, separator = ""):
+def letter_grid_file(file, separator = ""):
     lines = file.read().splitlines()
-    grid = [['' for x in range(len(lines[0]))] for y in range(len(lines))]
+    return letter_grid(lines, separator)
+
+def number_grid_file(file, separator = ""):
+    lines = file.read().splitlines()
+    return number_grid(lines, separator)
+
+def alphabet_scale_grid_file(file, separator=""):
+    global alphabet
+    lines = file.read().splitlines()
+    return alphabet_scale_grid(lines, separator)
+
+def letter_grid(lines, separator = ""):
+    first_line = lines[0].replace(separator, "")
+    grid = [['' for x in range(len(first_line))] for y in range(len(lines))]
     for y in range(len(lines)):
         line = list(lines[y]) if separator == "" else lines[y].split(separator)
         for x in range(len(line)):
             grid[y][x] = line[x]
     return grid
 
-def number_grid(file, separator = ""):
-    lines = file.read().splitlines()
-    grid = np.zeros((len(lines), len(lines[0])))
+def number_grid(lines, separator = ""):
+    first_line = lines[0].replace(separator, "")
+    grid = np.zeros((len(lines), len(first_line)))
     for y in range(len(lines)):
         line = list(lines[y]) if separator == "" else lines[y].split(separator)
         for x in range(len(line)):
             grid[y][x] = int(line[x])
     return grid
 
-def alphabet_scale_grid(file, separator=""):
-    global alphabet
-    grid = letter_grid(file, separator)
+def alphabet_scale_grid(lines, separator = ""):
+    grid = letter_grid(lines, separator)
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x] in alphabet:
