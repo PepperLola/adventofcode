a6c6256ad927ccb391849e287666947e2c0acd89
PepperLola <PepperLola@users.noreply.github.com>
2024-12-02 15:03:34 -0800

    feat: 2017 day 1 to test workflow

---
+++ 2017/day1/main.py
@@ -0,0 +1,50 @@
+import sys
+import time
+import re
+import itertools
+from collections import defaultdict, deque
+import math
+import copy
+import numpy as np
+from util import format_time, number_grid
+
+# shared variables here
+
+# part 1, takes in lines of file
+def p1(file, content, lines):
+    line = lines[0]
+    ret = 0
+    for i in range(len(line)):
+        c1, c2 = int(line[i]), int(line[(i+1)%len(line)])
+        if c1 == c2:
+            ret += c1
+    return ret
+
+# part 2, takes in lines of file
+def p2(file, content, lines):
+    line = lines[0]
+    ret = 0
+    for i in range(len(line)):
+        c1, c2 = int(line[i]), int(line[(i+len(line)//2)%len(line)])
+        if c1 == c2:
+            ret += c1
+    return ret
+
+filename = "input.txt"
+
+if "test" in sys.argv:
+    filename = "test.txt"
+
+with open(filename, "r") as f:
+    content = f.read()
+    lines = content.splitlines()
+    t = time.perf_counter_ns()
+    a = p1(f, content, lines)
+    dur = time.perf_counter_ns() - t
+
+    print("\033[0;33m├ Part 1:\033[22m\033[49m \033[1;93m%s\033[0m \033[3mtook\033[23m \033[3;92m%s\033[0m" % (a, format_time(dur)))
+
+    t = time.perf_counter_ns()
+    a = p2(f, content, lines)
+    dur = time.perf_counter_ns() - t
+    print("\033[0;33m└ Part 2:\033[22m\033[49m \033[1;93m%s\033[0m \033[3mtook\033[23m \033[3;92m%s\033[0m" % (a, format_time(dur)))
