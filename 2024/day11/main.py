5c2e5d6e00c8f48f1f4151b6478b07bc9d2b104c
PepperLola <PepperLola@users.noreply.github.com>
2024-12-10 21:40:30 -0800

    feat: day 11 846/1666

---
+++ 2024/day11/main.py
@@ -0,0 +1,62 @@
+import sys
+import time
+import re
+import itertools
+from collections import defaultdict, deque, Counter
+import math
+import copy
+import numpy as np
+import networkx as nx
+from util import format_time, number_grid
+
+# shared variables here
+stones = []
+
+def blink(counts):
+    ret = Counter()
+    for stone, count in counts.items():
+        if stone == "0":
+            ret["1"] += count
+        elif len(stone) % 2 == 0:
+            v1, v2 = str(int(stone[:len(stone)//2])), str(int(stone[len(stone)//2:]))
+
+            ret[v1] += count
+            ret[v2] += count
+        else:
+            ret[str(int(stone) * 2024)] += count
+    return ret
+
+# part 1, takes in lines of file
+def p1(file, content, lines):
+    global stones
+    stones = lines[0].split()
+    counts = Counter(stones)
+    for _ in range(25):
+        counts = blink(counts)
+    return sum(counts.values())
+
+# part 2, takes in lines of file
+def p2(file, content, lines):
+    counts = Counter(stones)
+    for _ in range(75):
+        counts = blink(counts)
+    return sum(counts.values())
+
+filename = "input.txt"
+
+if "test" in sys.argv:
+    filename = "test.txt"
+
+with open(filename, "r") as f:
+    content = f.read()
+    lines = content.splitlines()
+    t = time.perf_counter_ns()
+    a = p1(f, content, lines)
+    dur = time.perf_counter_ns() - t
+
+    print("\033[0;33m├ Part 1:\033[22m\033[49m \033[1;93m%s\033[0m \033[3mtook\033[23m \033[3;92m%s\033[0m" % (a, format_time(dur)))
+
+    t = time.perf_counter_ns()
+    a = p2(f, content, lines)
+    dur = time.perf_counter_ns() - t
+    print("\033[0;33m└ Part 2:\033[22m\033[49m \033[1;93m%s\033[0m \033[3mtook\033[23m \033[3;92m%s\033[0m" % (a, format_time(dur)))
